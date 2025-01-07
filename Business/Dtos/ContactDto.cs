7e15c9ecd34fc72e948e689a841bef8fa0b325a3
Maria Falk <mariacelf@gmail.com>
2025-01-07 17:03:24 +0100

    Added a contactDTO and model with needed properties. Created a GUIDgenerator helper.

---
+++ Business/Dtos/ContactDto.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Business.Dtos
+{
+
+    //A DTO, without logic, used to transfer data.
+    public class ContactDto
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string StreetAddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+
+    }
+}
---
+++ Business/Helpers/GuidGenerator.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Business.Helpers
+{
+    //Generates a new GUID for the Id property of the Contact class.
+    public static class GuidGenerator
+    {
+        public static string GenerateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
