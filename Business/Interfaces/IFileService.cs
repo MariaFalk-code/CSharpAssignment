3ba613649d15f9d11567dd17029e8773e7565506
Maria Falk <mariacelf@gmail.com>
2025-01-09 19:35:14 +0100

    Added methods and comments to IFileService.

--- Business/Interfaces/IFileService.cs
+++ Business/Interfaces/IFileService.cs
@@ -1,8 +1,23 @@
 ﻿
 
-namespace Business.Interfaces
+namespace Business.Interfaces;
+
+public interface IFileService
 {
-    public interface IFileService
-    {
-    }
+    /// <summary>
+    /// Saves a list of objects to a file.
+    /// </summary>
+    /// <typeparam name="T">Represents the type of listed objects.</typeparam>
+    /// <param name="list">The list to save</param>
+    
+    void SaveListToFile<T>(List<T> list);
+
+    /// <summary>
+    /// Reads a list of objects from a file.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to read from the file.</typeparam>
+    /// <param name="filePath">The filepath to read from</param>
+    /// <returns>A list of objects read from the file></returns>
+
+    List<T> ReadListFromFile<T>(string filePath);
 }
