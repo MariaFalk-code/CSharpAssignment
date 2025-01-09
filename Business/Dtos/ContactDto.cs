784c48ac6251ceb135d2b9c8b8ca205c9d0f5272
Maria Falk <mariacelf@gmail.com>
2025-01-09 16:09:39 +0100

    Created a ContactFactory with methods and comments. Added interfaces IContactService and IFileService.

--- Business/Dtos/ContactDto.cs
+++ Business/Dtos/ContactDto.cs
@@ -12,6 +12,7 @@ public class ContactDto
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string StreetAddress { get; set; } = null!;
+    public string StreetNumber { get; set; } = null!;
     public string City { get; set; } = null!;
     public string PostalCode { get; set; } = null!;
 
---
+++ Business/Factories/ContactFactory.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Business.Dtos;
+using Business.Models;
+using Business.Utilities;
+
+namespace Business.Factories;
+
+/// <summary>
+/// Provides methods for creating instances of <see cref="ContactRegistrationForm"/> and <see cref="ContactDto"/>
+/// </summary>
+public class ContactFactory
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="ContactRegistrationForm"/>
+    /// </summary>
+    public static ContactRegistrationForm CreateContactRegistrationForm()
+    {
+        return new ContactRegistrationForm();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="ContactDto"/> from a given <see cref="ContactRegistrationForm"/>.
+    /// </summary>
+    /// <param name="form">The form containing contact information to populate the DTO.</param>
+    /// <returns>A new <see cref="ContactDto"/> populated with the contact details from the form.</returns>
+    public static ContactDto Create(ContactRegistrationForm form)
+    {
+        return new ContactDto
+        {
+            Id = GuidGenerator.GenerateGuid(),
+            FirstName = form.FirstName,
+            LastName = form.LastName,
+            Email = form.Email,
+            PhoneNumber = form.PhoneNumber,
+            StreetAddress = form.StreetAddress,
+            StreetNumber = form.StreetNumber,
+            City = form.City,
+            PostalCode = form.PostalCode
+        };
+    }
+}
